2a3456a0430f7600d167795d24c93f1c8a0c7972
fabien0102 <fabien0102@planee.fr>
2017-04-15 15:31:15 +0200

    Add gatsby-link example

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,9 +1,19 @@
+import * as Link from "gatsby-link";
 import * as React from "react";
 
 export default class DefaultLayout extends React.Component<HTMLDivElement, void> {
   render() {
     return (
       <div id="layout">
+        <div>
+          <Link to="/" activeStyle={{color: "red"}} onlyActiveOnIndex={true}>
+            Home
+          </Link>
+          |
+          <Link to="/about" activeStyle={{color: "red"}}>
+            About
+          </Link>
+        </div>
         {/* Render children pages */}
         {this.props.children}
       </div>
