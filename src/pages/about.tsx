d86afafff92f47b5ba685ebafe59214f2fc5df0e
fabien0102 <fabien0102@planee.fr>
2017-04-26 20:04:02 +0200

    Add contact info into about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,29 @@
 import * as React from "react";
-import Title from "../components/Title";
+import { Header, Container, Segment, Icon } from "semantic-ui-react";
 
-export default () =>
-  <div>
-    <Title>About page</Title>
-  </div>;
+export default () => {
+  return (
+    <Container>
+      <Segment vertical>
+        <Header as="h2">
+          <Icon name="info circle" />
+          <Header.Content>
+            About
+          </Header.Content>
+        </Header>
+      </Segment>
+      <Segment vertical>
+        <p>
+          This starter was created by @fabien0102.
+        </p>
+        <p>
+          For any question, I'm on <a href="https://discord.gg/2bz8EzW" target="blank">discord #reactiflux/gatsby</a>
+        </p>
+        <p>
+          For any issues, any PR are welcoming
+          <a href="https://github.com/fabien0102/gatsby-starter/issues" target="blank"> on this repository</a>
+        </p>
+      </Segment>
+    </Container>
+  );
+};
