171cd131f51393dfbc4521137f605c099691f157
fabien0102 <fabien0102@planee.fr>
2017-05-03 00:37:35 +0200

    Update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,19 +47,21 @@ module.exports = {
     `gatsby-plugin-typescript`,
 
     // This plugin takes your configuration and generates a
-    // web manifest file so Gatsbygram can be added to your
+    // web manifest file so your website can be added to your
     // homescreen on Android.
+    /* eslint-disable camelcase */
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Gatsby website`,
-        shortName: `Gatsby website`,
-        startUrl: `/`,
-        backgroundColor: `#f7f7f7`,
-        themeColor: `#191919`,
+        short_name: `Gatsby website`,
+        start_url: `/`,
+        background_color: `#f7f7f7`,
+        theme_color: `#191919`,
         display: `minimal-ui`
       }
     },
+    /* eslint-enable camelcase */
 
     // This plugin generates a service worker and AppShell
     // html file so the site works offline and is otherwise
