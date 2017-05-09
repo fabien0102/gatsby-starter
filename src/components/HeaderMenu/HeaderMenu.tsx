12f8ab6c8939ace62973b1c9d5cd92b4a7cde566
fabien0102 <fabien0102@planee.fr>
2017-05-09 11:02:54 +0200

    Deal with menu on mobile

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -7,15 +7,15 @@ interface HeaderMenuProps extends MenuProps {
   toggleSidebar: () => void;
 }
 
-export default ({ items, pathname, Link, inverted, toggleSidebar}: HeaderMenuProps) => {
+export default ({ items, pathname, Link, inverted, toggleSidebar }: HeaderMenuProps) => {
   return (
     <Container>
       <Menu size="large" pointing secondary inverted={inverted}>
-        <Menu.Item as="a" icon="sidebar" onClick={toggleSidebar}/>
-        <Menu.Item><Icon name="spy" size="big"/></Menu.Item>
+        <Menu.Item as="a" className="mobile only" icon="sidebar" onClick={toggleSidebar} />
+        <Menu.Item className="mobile hidden"><Icon name="spy" size="big" /></Menu.Item>
         {items.map((item) => {
           const active = (item.exact) ? pathname === item.path : pathname.startsWith(item.path);
-          return <Menu.Item as={Link} name={item.name} to={item.path} active={active} />;
+          return <Menu.Item as={Link} className="mobile hidden" name={item.name} to={item.path} active={active} />;
         })}
       </Menu>
     </Container>
