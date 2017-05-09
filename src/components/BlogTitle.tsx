b66f55811c50c03fb124ccc8f1fb336bd2406002
fabien0102 <fabien0102@planee.fr>
2017-05-09 10:58:34 +0200

    Fix indent

--- src/components/BlogTitle.tsx
+++ src/components/BlogTitle.tsx
@@ -4,15 +4,15 @@ import { Header, Segment, Icon } from "semantic-ui-react";
 export default () => {
   return (
     <Segment vertical>
-        <Header as="h2">
-          <Icon name="newspaper" />
-          <Header.Content>
-            Blog
+      <Header as="h2">
+        <Icon name="newspaper" />
+        <Header.Content>
+          Blog
             <Header.Subheader>
-              All about this starter kit
+            All about this starter kit
             </Header.Subheader>
-          </Header.Content>
-        </Header>
-      </Segment>
+        </Header.Content>
+      </Header>
+    </Segment>
   );
 };
