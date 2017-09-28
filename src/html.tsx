db6ff74d00d41bb5853cb35cebc8545a83167787
fabien0102 <fabien0102@planee.fr>
2017-09-28 12:03:05 +0200

    Fix createClass warning

--- src/html.tsx
+++ src/html.tsx
@@ -21,41 +21,39 @@ interface HtmlProps {
 }
 
 // Use `module.exports` to be compliante with `webpack-require` import method
-module.exports = React.createClass<HtmlProps, void>({
-  render() {
-    const head = Helmet.rewind();
+module.exports = (props: HtmlProps) => {
+  const head = Helmet.rewind();
 
-    const css = (process.env.NODE_ENV === `production`) ?
-      <style
-        id="gatsby-inlined-css"
-        dangerouslySetInnerHTML={{ __html: styles }}
-      />
-      : null;
+  const css = (process.env.NODE_ENV === `production`) ?
+    <style
+      id="gatsby-inlined-css"
+      dangerouslySetInnerHTML={{ __html: styles }}
+    />
+    : null;
 
-    return (
-      <html lang="en">
-        <head>
-          {this.props.headComponents}
-          <title>My website</title>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
-          />
-          {head.title.toComponent()}
-          {head.meta.toComponent()}
-          {head.link.toComponent()}
-          {css}
-        </head>
-        <body>
-          <div
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
-          />
-          {this.props.postBodyComponents}
-        </body>
-      </html>
-    );
-  },
-});
+  return (
+    <html lang="en">
+      <head>
+        {props.headComponents}
+        <title>My website</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
+        {head.title.toComponent()}
+        {head.meta.toComponent()}
+        {head.link.toComponent()}
+        {css}
+      </head>
+      <body>
+        <div
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+};
