66ef42b90a5904abf574b30b69d441c6c6666d76
fabien0102 <fabien0102@planee.fr>
2017-09-29 11:18:09 +0200

    Fix replaceRouterComponent

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import * as React from "react";
+import { Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { store } from "./src/store";
 
-exports.replaceRouterComponent = ({ history }: any) =>
+exports.replaceRouterComponent = ({ history }) =>
   ({ children }) =>
     <Provider store={store} >
-      <Router history={history}>{children} </Router>
+      <Router history={history}>{children}</Router>
     </Provider>;
