fa13097160987e237648433a05c16887bb9543c0
fabien0102 <fabien0102@planee.fr>
2017-10-10 10:36:21 +0200

    Add integrity tests for blog posts

--- test/data-integrity.test.js
+++ test/data-integrity.test.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-undef, max-nested-callbacks */
 const fs = require('fs');
 const path = require('path');
+const matter = require('gray-matter');
+const _ = require('lodash');
+const authors = require('../data/author.json');
 
 describe('data integrity', () => {
   describe('authors', () => {
-    const authors = require('../data/author.json');
     const requiredFields = ['id', 'bio', 'avatar', 'twitter', 'github'];
 
     authors.forEach(author => {
@@ -20,6 +22,30 @@ describe('data integrity', () => {
         it('should have avatar image in the repo', () => {
           const avatarPath = path.join('data/', author.avatar);
           expect(fs.existsSync(avatarPath)).toBeTruthy();
+        });
+      });
+    });
+  });
+
+  describe('blog posts', () => {
+    const posts = fs.readdirSync('data/blog');
+    const validators = [
+      {key: 'title', validator: _.isString},
+      {key: 'createdDate', validator: val => _.isDate(new Date(val))},
+      {key: 'updatedDate', validator: val => _.isDate(new Date(val))},
+      {key: 'author', validator: val => _.map(authors, 'id').includes(val)},
+      {key: 'tags', validator: _.isArray},
+      {key: 'image', validator: (val, post) => fs.existsSync(`data/blog/${post}/${val}`)},
+      {key: 'draft', validator: _.isBoolean}
+    ];
+
+    posts.forEach(post => {
+      describe(`${post}`, () => {
+        const {data} = matter.read(`data/blog/${post}/index.md`);
+        validators.forEach(field => {
+          it(`should have correct format for ${field.key}`, () => {
+            expect(field.validator(data[field.key], post)).toBeTruthy();
+          });
         });
       });
     });
