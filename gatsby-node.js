e5b7276eb0799838ebe3d370bfa3b4c037b787bc
Resi Respati <resi.respati@compare88.net>
2017-11-13 11:05:49 +0700

    Ensure Gatsby builds production React from TypeScript

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,26 @@ const {kebabCase, uniq, get, compact, times} = require('lodash');
 const POSTS_PER_PAGE = 10;
 const cleanArray = arr => compact(uniq(arr));
 
+// Add Gatsby's extract-graphql Babel plugin (we'll chain it with babel-loader)
+const extractQueryPlugin = path.resolve(
+  __dirname,
+  `node_modules/gatsby/dist/utils/babel-plugin-extract-graphql.js`
+)
+
+// Temporary workaround to ensure Gatsby builds minified, production build of React.
+// https://github.com/fabien0102/gatsby-starter/issues/39#issuecomment-343647558
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  if (stage === 'build-javascript') {
+    config.loader('typescript', {
+      test: /\.tsx?$/,
+      loaders: [
+        `babel-loader?${JSON.stringify({ presets: ['babel-preset-env'], plugins: [extractQueryPlugin] })}`,
+        'ts-loader'
+      ]
+    })
+  }
+}
+
 // Create slugs for files.
 // Slug will used for blog page path.
 exports.onCreateNode = ({node, boundActionCreators, getNode}) => {
