721fd58fcd6ec2aefcd83dc309cba272029dd58b
Marcin <mmpoiu@gmail.com>
2018-08-26 17:58:37 +0200

    migration to v2: chnaged replaceRouterComponent to wrapRootElement and remooving react-router

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
-import { Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { store } from "./src/store";
 
-exports.replaceRouterComponent = ({ history }) =>
-  ({ children }) =>
+export const wrapRootElement = ({ element }) =>
     <Provider store={store} >
-      <Router history={history}>{children}</Router>
+      {element}
     </Provider>;
