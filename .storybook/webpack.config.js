15d899c2d243e0262fb1f707718dc0aa1593e661
Benson Trent <bensontrent@gmail.com>
2019-05-20 08:31:51 -0700

    Update Dependencies: Webpack & Storybook ...

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,15 @@
-const path = require("path");
-const genDefaultConfig = require("@storybook/react/dist/server/config/defaults/webpack.config.js");
+const path = require('path');
 
-module.exports = (baseConfig, env) => {
-  const config = genDefaultConfig(baseConfig, env);
+module.exports = async ({ config, mode }) => {
+    // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
 
   // Add typescript loader
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     include: path.resolve(__dirname, "../src"),
-    loader: require.resolve("ts-loader"),
+    loader: "ts-loader",
     options: {
       configFile: ".storybook/tsconfig.json"
     }
@@ -19,7 +20,7 @@ module.exports = (baseConfig, env) => {
   config.module.rules.push({
     test: /\.md$/,
     include: path.resolve(__dirname, "../src"),
-    loader: require.resolve("raw-loader")
+    loader: "raw-loader"
   });
   config.resolve.extensions.push(".md");
 
